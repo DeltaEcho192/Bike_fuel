b773eb2639004c033bad53d165063440e403aee3
admin <anthony@durrer.co.za>
2020-06-20 03:38:13 +0200

    JSON Parsing Error

---
+++ public-html/directions.js
@@ -0,0 +1,11 @@
+
+const url='https://maps.googleapis.com/maps/api/directions/json?origin=2+Faure+St,+Kromrivier,+Stellenbosch,+7600&destination=8+Groeneweide+Rd,+Universiteits+Oord,+Stellenbosch,+7600&key=AIzaSyAIf-vJKm6y4vhqsCFdMkuRYIOjb8Q8rxM';
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function() {
+if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+    console.log("Success");
+    var apireturn = JSON.parse(xmlHttp.responseText);
+    document.getElementById("output").innerHTML = apireturn
+}
+xmlHttp.open("GET", url, true);
+xmlHttp.send();
