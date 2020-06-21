9fe6cfc71ef3622b94cf80fd0dfeffab27b2b4e4
admin <anthony@durrer.co.za>
2020-06-22 01:50:22 +0200

    Calculation working

--- public-html/directions.js
+++ public-html/directions.js
@@ -3,9 +3,28 @@ const url='https://maps.googleapis.com/maps/api/directions/json?origin=2+Faure+S
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.onreadystatechange = function() {
 if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-    console.log("Success");
     var apireturn = JSON.parse(xmlHttp.responseText);
-    document.getElementById("output").innerHTML = apireturn
+    document.getElementById("output").innerHTML = apireturn.routes[0].legs[0].distance.value
+    var distance = apireturn.routes[0].legs[0].distance.value
+    var distDisplay = apireturn.routes[0].legs[0].distance.text
+    var time = apireturn.routes[0].legs[0].duration.text
+    console.log(distance)
+    console.log(distDisplay)
+    console.log(time)
+    calculations(distance,time)
 }
 xmlHttp.open("GET", url, true);
 xmlHttp.send();
+
+function calculations(distance,time)
+{
+    var fuelEff = 0.00006
+    var fuelPrice = 13.20
+    
+    var fuelUsage = fuelEff * distance
+    console.log(fuelUsage) 
+    var fnlCost = fuelUsage * fuelPrice
+    console.log(fnlCost)
+
+}
+
