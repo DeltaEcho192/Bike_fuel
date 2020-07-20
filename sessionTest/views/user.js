af10d02bb3f3cf15bb1c8bfb23f50fb169d396a5
admin <anthony@durrer.co.za>
2020-07-20 14:30:01 +0200

    Added Search function to User Page and CSS Cleanup

--- sessionTest/views/user.js
+++ sessionTest/views/user.js
@@ -50,3 +50,23 @@ async function fuelGet() {
         addRow(routeData)
     });
 }
+
+function search() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("filterTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
